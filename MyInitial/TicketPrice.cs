f160de2d18e4029b980f278bb13ff7bb3511fe0f
bradleym <bradley.mcghee@student.tafesa.edu.au>
2018-03-05 14:04:50 +1030

    commit the child discount changes

--- MyInitial/TicketPrice.cs
+++ MyInitial/TicketPrice.cs
@@ -16,7 +16,8 @@ namespace Ticketing
         const decimal mdecBalcony = 35.5m;
         const decimal mdecGeneral = 28.75m;
         const decimal mdecBox = 62.0m;
-        const decimal mdecDiscount = 5.0m; 
+        const decimal mdecDiscount = 5.0m;
+        const decimal mdecDiscount2 = 10.0m;
 
         private int Section
         {
